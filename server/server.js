33f944ac9ebb9b65ddae44797c2f32a6d832a61e
Ahmad Ali <ali333291@gmail.com>
2018-03-31 22:51:53 +0500

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -15,7 +15,10 @@ var {
 
 var app = express();
 app.use(bodyParser.json());
+
+
 // Routes
+// -- Add a new todo
 app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
@@ -28,12 +31,25 @@ app.post('/todos', (req, res) => {
     });
 });
 
+// -- View all todos
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({
+            todos
+        });
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
 
 
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
-}); 
+});
 
 
-module.exports = {app};
+module.exports = {
+    app
+};
