9201e0ee82148813633be42cf7da01672deb4e98
Ahmad Ali <ali333291@gmail.com>
2018-04-09 17:54:25 +0500

    Setup app for heroku

--- server/server.js
+++ server/server.js
@@ -15,6 +15,7 @@ var {
 // Configuration
 
 var app = express();
+const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 
@@ -76,8 +77,8 @@ app.get('/todos/:id', (req, res) => {
 
 
 
-app.listen(3000, () => {
-    console.log('Started on port 3000');
+app.listen(port, () => {
+    console.log(`Started up at port ${port}`);
 });
 
 
