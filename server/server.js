7d98c0c9c56a5eee0ac8dd29f8db4a9278943a4d
microcyberz <ali333291@gmail.com>
2018-04-17 23:10:45 +0500

    JWTs, Hashing, generating auth tokens

--- server/server.js
+++ server/server.js
@@ -1,138 +1,117 @@
 require('./config/config');
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
-const { ObjectID } = require('mongodb');
+const {ObjectID} = require('mongodb');
 
-var {
-    mongoose
-} = require('./db/mongoose');
-var {
-    Todo
-} = require('./models/todo');
-var {
-    User
-} = require('./models/user');
-
-// Configuration
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
 var app = express();
 const port = process.env.PORT;
+
 app.use(bodyParser.json());
 
-
-// Routes
-// -- Add a new todo
 app.post('/todos', (req, res) => {
-    var todo = new Todo({
-        text: req.body.text
-    });
+  var todo = new Todo({
+    text: req.body.text
+  });
 
-    todo.save().then((doc) => {
-        res.send(doc);
-    }, (e) => {
-        res.status(400).send(e);
-    });
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
-// -- Add a new user
-app.post('/users', (req, res) => {
-    var user = new User({
-        email: req.body.email
-    });
-    user.save().then((doc) => {
-        res.send(doc);
-    }, (e) => {
-        res.status(400).send(e);
-    });
-});
-
-// -- View all todos
 app.get('/todos', (req, res) => {
-    Todo.find().then((todos) => {
-        res.send({
-            todos
-        });
-    }, (e) => {
-        res.status(400).send(e);
-    });
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
-// -- View one todo
 app.get('/todos/:id', (req, res) => {
-    var id = req.params.id;
-    if (!ObjectID.isValid(id)) {
-        return res.status(404).send();
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
     }
 
-    Todo.findById(id).then((todo) => {
-        if (!todo) {
-            return res.status(404).send()
-        }
-        res.send({ todo });
-
-    }).catch((e) => {
-        res.status(400).send();
-    });
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
-
-// Deleting 
 app.delete('/todos/:id', (req, res) => {
-    var id = req.params.id;
-    if (!ObjectID.isValid(id)) {
-        return res.status(404).send();
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
     }
 
-    Todo.findByIdAndRemove(id).then((todo) => {
-        if (!todo) {
-            return res.status(404).send();
-        }
-        res.send({ todo });
-    }).catch((e) => {
-        res.status(400).send();
-    });
-
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
-
-// Updating
 app.patch('/todos/:id', (req, res) => {
-    var id = req.params.id;
-    var body = _.pick(req.body, ['text', 'completed']);
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
 
-    // validate the id
-    if (!ObjectID.isValid(id)) {
-        return res.status(404).send();
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
     }
 
-    // timestamp of completion
-    if (_.isBoolean(body.completed) && body.completed) {
-        body.completedAt = new Date().getTime();
-    } else {
-        body.completed = false;
-        body.completedAt = null;
-    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
+});
 
-    // update
-    Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo)=>{
-        if(!todo){
-            return res.status(404).send();
-        }
+// POST /users
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
 
-        res.send({todo});
-    }).catch((e)=>{
-        res.status(400).send();
-    });
-
-
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user)
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
 });
 
 app.listen(port, () => {
-    console.log(`Started up at port ${port}`);
+  console.log(`Started up at port ${port}`);
 });
 
-
-module.exports = {
-    app
-};
+module.exports = {app};
